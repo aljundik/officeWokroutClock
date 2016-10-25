335bc8508edf6523650e14778b1caefd2a93e4a2
Hafiz <hafizm.shehzad@gmail.com>
2016-10-25 17:12:50 +0300

    front

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-var myApp= angular.module('starter', ['ionic', 'starter.controllers', 'angular-svg-round-progressbar'])
+var myApp= angular.module('starter', ['ionic', 'starter.controllers','starter.services', 'angular-svg-round-progressbar'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
